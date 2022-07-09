cca759e6ee61da6f0b86d8f887242d62a799c1f1
Alex Goodall <alexandergoodall123@gmail.com>
2022-07-09 13:18:57 +0100

    initial

---
+++ center.tmux
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+source "$CURRENT_DIR/scripts/helpers.sh"
+
+set_tmux_option() {
+	local option="$1"
+	local value="$2"
+	tmux set-option -gq "$option" "$value"
+}
+
+do_interpolation() {
+	local interpolated="$1"
+    local interp="pane_status_center"
+    local script="$CURRENT_DIR/scripts/padded_status.sh #{pane_width}"
+    interpolated=${interpolated/$interp/$script}
+	echo "$interpolated"
+}
+
+update_tmux_option() {
+	local option="$1"
+	local option_value="$(get_tmux_option "$option")"
+	local new_option_value="$(do_interpolation "$option_value")"
+	set_tmux_option "$option" "$new_option_value"
+}
+
+main() {
+    update_tmux_option "pane-border-format"
+}
+
+main
---
+++ scripts/padded_status.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+source "$CURRENT_DIR/helpers.sh"
+
+surround_l_default='['
+surround_r_default=']'
+
+surround_l=""
+surround_r=""
+
+get_settings() {
+    surround_l=$(get_tmux_option "@padd_surround_l" "$surround_l_default")
+    surround_r=$(get_tmux_option "@padd_surround_r" "$surround_r_default")
+}
+
+get_padded() {
+    local w=$1
+    local ind=$2
+    local pad_char='─'
+    local str=''
+    local digits=$(echo -n $ind | wc -m | tr -d ' ')
+    local line_amount=$((w - 2 - digits))
+    local text_len=$((digits + 2))
+    local start=$((line_amount / 2))
+    for i in $(seq 0 $line_amount) ; do
+        if [ $i = $start ] ; then
+            str+="$surround_l"
+            str+="$ind"
+            str+="$surround_r"
+        fi
+        str+="$pad_char"
+    done
+    echo $str
+}
+
+main() {
+    get_settings
+    get_padded $1 $2
+}
+
+main $1 $2
